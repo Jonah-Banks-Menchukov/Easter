c4a98777153cf91385efed53abf452dc25a441f3
Jonah-Banks-Menchukov <jalejobanks@gmail.com>
2025-09-03 18:29:59 +0000

    next

--- Easter.java
+++ Easter.java
@@ -2,6 +2,21 @@
 // This will calculate Easter based on a given year after 1500
 public class Easter{
     public static void main (String[] args){
-        
+       int y=2026;
+       int a= y%19;
+       int b=y/100;
+       int c=y%100;
+       int d=b/4;
+       int e=b%4;
+       int f=(b+8)/25;
+       int g=(b-f+1)/3;
+       int h=(19*a+b
+       System.out.println(a);
+       System.out.println(b);
+       System.out.println(c);
+       System.out.println(d);
+       System.out.println(e);
+       System.out.println(f);
+       System.out.println(g);
     }
 }
